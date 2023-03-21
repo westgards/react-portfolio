54d0daf535a28c86789497d037f3db3d63c4ce21
nico <westernfarthing@gmail.com>
2023-03-21 00:14:28 -0500

    initial file structure

---
+++ react-portfolio/src/utils/helpers.js
@@ -0,0 +1,12 @@
+export function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+export function validateEmail(email) {
+    var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1-3}\.[0-9]{1-3}\])|(([a-zA-Z\-0-9]+
+        return re.test(String(email).toLowerCase());
+}
+
+export function removeHyphensAndCaptialize(string) {
+    return string.replace(/-/g, ' ').replace(/(^\w|\s\w)/g, m => m.toUpperCase())
+}
