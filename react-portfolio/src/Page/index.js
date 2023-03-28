24be9becba90df4e5c0e17afd19f86394e609997
nico <westernfarthing@gmail.com>
2023-03-27 22:12:26 -0500

    moved to src folder

--- react-portfolio/src/Page/index.js
+++ react-portfolio/src/Page/index.js
@@ -4,7 +4,7 @@ import About from "../components/About";
 import Portfolio from "../components/Portfolio";
 import Contact from "../components/Contact";
 import Resume from "../components/Resume";
-import { capitalizeFirstLetter } from "../../utils/helpers";
+import { capitalizeFirstLetter } from "../utils/helpers";
 
 function Page({ currentPage }) {
   const renderPage = () => {
